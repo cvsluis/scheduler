211174382b82f1d8a61d21415f75f376cc64ac13
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-26 21:42:11 -0700

    feat: transition with replace functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode);
-    setHistory(prev => [...prev, newMode]);
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
   }
 
   function back() {
