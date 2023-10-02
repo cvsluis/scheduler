3e5df4c7cce012f43528140f2030ca9a29ad50bb
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-10-02 12:52:35 -0700

    feat: write advance test for Application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { render, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  waitForElement,
+  fireEvent,
+  getByText,
+  getAllByTestId,
+  getByPlaceholderText,
+  getByAltText,
+  queryByText,
+} from "@testing-library/react";
 import Application from "components/Application";
 
 describe("Application", () => {
@@ -14,5 +23,42 @@ describe("Application", () => {
       fireEvent.click(getByText("Tuesday"));
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
+  });
+
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
